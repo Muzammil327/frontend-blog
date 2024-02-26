4bd6fb343d48b75c03a8de3f0cbd85dbdfb4f750
Muzammil327 <mmuzammiloff327@gmail.com>
2024-02-26 18:02:36 +0500

    updated home and post slug

--- src/components/hero/page.tsx
+++ src/components/hero/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Hero(props: { title: string }) {
-  return <h1 className="py-40 capitalize bg-red-100 mb-10 text-black text-3xl font-bold container mx-auto c">{props.title}</h1>;
+  return <h1 className="py-40 capitalize bg-red-100 mb-10 text-black text-3xl text-center font-bold container mx-auto c">{props.title}</h1>;
 }
